9f9c9a566863aaa39f41b004576e3f7b24f34e58
Aqulin-Jebha <126853902+Aqulin-Jebha@users.noreply.github.com>
2025-12-09 18:31:00 +0530

    completed practice tasks.js

---
+++ Day 17/practical tasks.js
@@ -0,0 +1,69 @@
+// task 1 Age Checker
+let age=88;
+if(age<=15){
+    console.log("Child")
+}else if(age>=18 && age<=34){
+      console.log("Adult")
+}else{
+    console.log("Senior")
+
+}
+
+//task2 calculator
+let a=10;
+let b=20;
+let op=("");
+
+ if(op=="+"){
+    console.log(a+b)
+ } else if(op=="-"){
+    console.log(a-b)
+ } else if(op=="*"){
+    console.log(a*b)
+ }else if(op=="/"){
+    console.log(a/b)
+ }
+
+ //task 3 Login Logic
+ let username="Aqulin";
+ let password="Aqulin";
+ if(username==password){
+    console.log("welcome")
+ }else{
+    console.log("Invalid User")
+ }
+
+ //task4 Day Finder with Switch
+ let day = ("");
+ if(day=="1"){
+    console.log("Monday")
+ }else if(day=="2"){
+    console.log("Tuesday")
+ }else if(day=="3"){
+    console.log("Wednesday")
+ }else if(day=="4"){
+    console.log("Thursday")
+ }else if(day=="5"){
+    console.log("Friday")
+ }else if(day=="6"){
+    console.log("Saturday")
+ }else{
+    console.log("Sunday")
+ }
+
+ //task5 Student Grading System
+
+ let mark= 55;
+ if(mark >=90){
+    console.log("A+")
+ }else if(mark>=75){
+    console.log("A")
+ }else if(mark>=60){
+    console.log("B")
+ }else if(mark>=50){
+    console.log("D")
+ }else if(mark>=35){
+    console.log("E")
+ }else{
+    console.log("F")
+ }
---
+++ Day 17/practice tasks.js
@@ -0,0 +1,53 @@
+//task 1
+let a = 10;
+let b = 99;
+let c = a+b;
+console.log(c)
+
+var name ="Aqulin";
+console.log(name)
+
+const p =3.17;
+console.log(p)
+
+//task 2
+var m ="Hello"
+console.log(typeof m)
+
+let numb=35;
+console.log(typeof numb)
+
+let s=true;
+let r=false;
+console.log(typeof s)
+console.log(typeof r)
+
+let data=null;
+console.log(typeof data)
+
+//task3
+let x=22;
+let y =77;
+console.log("added value:" ,a+b)
+console.log("differece of a and b:", a-b)
+console.log("multiplied value:", a*b)
+console.log("division:", a/b)
+console.log("modulus:", a%b)
+console.log("Exponentiation: " + (a ** b));
+console.log("Increment: " + (a++));
+console.log("Decrement: " + (--b));
+
+//task 4
+let v="34";
+let w=34;
+let u=34
+console.log(u==w)
+console.log(v===u)
+
+//task5
+let e= 67;
+if (e%2==0){
+    console.log("even")
+}else{
+    console.log("odd")
+}
